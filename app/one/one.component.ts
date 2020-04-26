2374bc60a06afc6bb52439113202f31716fa5dca
Carlos Villa <cfvillahermosa@gmail.com>
2020-04-26 14:29:19 +0200

    ChangeDetectionStrategy.OnPush and Immutability

--- app/one/one.component.ts
+++ app/one/one.component.ts
@@ -1,22 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: 'example-one',
-  styles: [`
-    .example-one {
-      background: #9f72e6;
-      font-size: 19px;
-      color: #fff;
-      margin-bottom: 50px;
-      padding: 10px 20px;
-    }
-  `],
+  selector: "example-one",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [
+    `
+      .example-one {
+        font-size: 19px;
+        margin-bottom: 10px;
+      }
+    `,
+  ],
   template: `
     <div class="example-one">
-      Example One
+      <h3>On Push</h3>
+      <h4>{{ user.name }}</h4>
+      <h5>{{ user.age }} years old</h5>
+      {{ user.location }} <br />
+      {{ user.email }}
+
+      <button (click)="update()">Internal update</button>
+      <p>* should not update</p>
     </div>
-  `
+  `,
 })
 export class ExampleOneComponent {
+  @Input()
+  user;
 
+  update() {
+    this.user.name = "Matt Skiba";
+  }
 }
--- app/two/two.component.ts
+++ app/two/two.component.ts
@@ -1,32 +1,34 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
-  selector: 'example-two',
-  encapsulation: ViewEncapsulation.Native, // shadow DOM
-  styles: [`
-    .example-two {
-      background: #9f72e6;
-      font-size: 19px;
-      color: #fff;
-      margin-bottom: 10px;
-      padding: 5px 7px;
-    }
-    .example-one {
-      border: 3px solid #9f72e6;
-      font-size: 14px;
-      color: #9f72e6;
-      padding: 5px 7px;
-    }
-  `],
+  selector: "example-two",
+  changeDetection: ChangeDetectionStrategy.Default,
+  styles: [
+    `
+      .example-two {
+        font-size: 19px;
+        margin-bottom: 10px;
+      }
+    `,
+  ],
   template: `
     <div class="example-two">
-      Example Two
+      <h3>Default</h3>
+      <h4>{{ user.name }}</h4>
+      <h5>{{ user.age }} years old</h5>
+      {{ user.location }} <br />
+      {{ user.email }}
+
+      <button (click)="update()">Internal update</button>
+      <p>* should update</p>
     </div>
-    <div class="example-one">
-      Example One!
-    </div>
-  `
+  `,
 })
 export class ExampleTwoComponent {
+  @Input()
+  user;
 
+  update() {
+    this.user.name = "Scott Raynor";
+  }
 }
