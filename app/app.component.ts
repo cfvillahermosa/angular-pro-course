2dfbc2e4eb564cb531baf29d7c37d9a874dc0c9c
Carlos Villa <cfvillahermosa@gmail.com>
2020-04-26 18:57:26 +0200

    Creating a custom structural Directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,25 +4,43 @@ import { Component } from "@angular/core";
   selector: "app-root",
   template: `
     <div>
-      <label>
-        Credit Card Number
-        <input
-          name="credit-card"
-          type="text"
-          placeholder="Enter your 16-digit card number"
-          credit-card
-        />
-      </label>
-      <label
-      tooltip="3 digits, back of your card"
-      #myTooltip="tooltip"><!-- to get the value and to get access to our tooltip because we use the exportAs "tooltip", we can then access this because we have bound it to the same element -->
-        Enter your security code
-        <span (mouseover)="myTooltip.show()" (mouseout)="myTooltip.hide()">
-          (?)
-        </span>
-        <input type="text"/>
-      </label>
+      <ul>
+        <li *myFor="let item of items; let i = index">
+          {{ i }} Member: {{ item.name | json }}
+        </li>
+        <template myFor [myForOf]="items" let-item let-i="index">
+          <li>
+            {{ i }} Member: {{ item.name | json }}
+          </li>
+        </template>
+      </ul>
     </div>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  items = [
+    {
+      name: "Mark Hoppus",
+      age: 44,
+      location: "California",
+    },
+    {
+      name: "Tom Delonge",
+      age: 41,
+      location: "California",
+    },
+    {
+      name: "Travis Barker",
+      age: 41,
+      location: "California",
+    },
+  ];
+  constructor() {
+    setTimeout(() => {
+      this.items = [
+        ...this.items,
+        { name: "Matt Skiba", age: 40, location: "California" },
+      ];
+    }, 2000);
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { CreditCardDirective } from "./credit-card/credit-card.directive";
-import { TooltipDirective } from './tooltip/tooltip.directive';
+import { AppComponent } from './app.component';
+import { MyForDirective } from './my-for/my-for.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardDirective,
-    TooltipDirective
+    MyForDirective
   ],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
