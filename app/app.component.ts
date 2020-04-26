990e2e321a1e970dde8d89efc4b0f4d139c7e88b
Carlos Villa <cfvillahermosa@gmail.com>
2020-04-26 02:27:45 +0200

    Destroying dynamic components

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,20 +4,31 @@ import {
   ComponentFactoryResolver,
   ViewChild,
   ViewContainerRef,
+  ComponentRef,
 } from "@angular/core";
 import { AuthFormComponent } from "./auth-form/auth-form.component";
 import { User } from "./auth-form/auth-form.interface";
 
 @Component({
   selector: "app-root",
+  styles: [`
+    button {
+      max-height: 35px;
+    }
+  `],
   template: `
     <div>
+      <button (click)="destroyComponent()">
+        Destroy
+      </button>
       <!-- Container div for adding components with create component method of ComponentFactoryResolver -->
       <div #entry></div>
     </div>
   `,
 })
 export class AppComponent implements AfterContentInit {
+  component: ComponentRef<AuthFormComponent>;
+
   // 1 - we create the ViewChild element that will contain dynamic components
   @ViewChild("entry", { read: ViewContainerRef }) entry: ViewContainerRef;
 
@@ -34,9 +45,13 @@ export class AppComponent implements AfterContentInit {
       AuthFormComponent
     );
     // 4 - we add into our container our component factory
-    const component = this.entry.createComponent(authFormFactory);
-    component.instance.title = 'Create account';
-    component.instance.submitted.subscribe(this.loginUser);
+    this.component = this.entry.createComponent(authFormFactory);
+    this.component.instance.title = "Create account";
+    this.component.instance.submitted.subscribe(this.loginUser);
+  }
+
+  destroyComponent() {
+    this.component.destroy();
   }
 
   loginUser(user: User) {
