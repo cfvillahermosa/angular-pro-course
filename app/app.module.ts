05cb6a4e958ac66415406417c49180c9f5ee5d3b
Carlos Villa <cfvillahermosa@gmail.com>
2020-04-26 14:31:19 +0200

    Lesson - Directives

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { ExampleOneComponent } from './one/one.component';
-import { ExampleTwoComponent } from './two/two.component';
 
+import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleOneComponent,
-    ExampleTwoComponent
+    CreditCardDirective
   ],
   imports: [
     BrowserModule
