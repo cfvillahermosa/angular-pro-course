1e3bf42adedadb1e12f870bda870bc893d6c7ded
Carlos Villa <cfvillahermosa@gmail.com>
2020-11-02 18:07:05 +0100

    Enabling route tracing

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,22 +7,11 @@ import { MailModule } from './mail/mail.module';
 
 import { AppComponent } from './app.component';
 
-export const ROUTES: Routes = [
-  { path: '**', redirectTo: 'folder/inbox' }
-];
+export const ROUTES: Routes = [{ path: '**', redirectTo: 'folder/inbox' }];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpModule,
-    MailModule,
-    RouterModule.forRoot(ROUTES, { enableTracing: true })
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpModule, MailModule, RouterModule.forRoot(ROUTES, { enableTracing: true })],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
