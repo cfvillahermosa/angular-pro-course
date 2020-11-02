c84f144ab98cdacbbe0d24e8392d856f3d0c302a
Carlos Villa <cfvillahermosa@gmail.com>
2020-11-02 18:55:14 +0100

    Subscribing to router events

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { NavigationEnd, Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
       <header>
-        <img src="/img/logo.svg">
+        <img src="/img/logo.svg" />
       </header>
       <div class="app__content">
         <nav>
-          <a
-            routerLink="folder/inbox"
-            routerLinkActive="active">
+          <a routerLink="folder/inbox" routerLinkActive="active">
             Inbox
           </a>
-          <a
-            routerLink="folder/trash"
-            routerLinkActive="active">
+          <a routerLink="folder/trash" routerLinkActive="active">
             Trash
           </a>
         </nav>
@@ -29,5 +25,11 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router) {}
-  ngOnInit() {}
+  ngOnInit() {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(event => {
+        console.log('*** event', event);
+      });
+  }
 }
