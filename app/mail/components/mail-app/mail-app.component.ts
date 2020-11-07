440281c43c52779bd7ef659deb4078d317c0336c
Carlos Villa <cfvillahermosa@gmail.com>
2020-11-07 02:16:00 +0100

    Auxiliary named router outlets

--- app/mail/components/mail-app/mail-app.component.ts
+++ app/mail/components/mail-app/mail-app.component.ts
@@ -5,7 +5,11 @@ import { Component } from '@angular/core';
   styleUrls: ['mail-app.component.scss'],
   template: `
     <div class="mail">
-      <router-outlet (activate)="onActivate($event)" (deactivate)="onDeactivate($event)"></router-outlet>
+      <router-outlet></router-outlet>
+    </div>
+    <div class="mail">
+      <!-- http://localhost:4000/folder/inbox(pane:message/1) -->
+      <router-outlet name="pane"></router-outlet>
     </div>
   `
 })
--- app/mail/mail.module.ts
+++ app/mail/mail.module.ts
@@ -3,17 +3,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MailAppComponent } from './components/mail-app/mail-app.component';
 import { MailItemComponent } from './components/mail-item/mail-item.component';
+import { MailViewComponent } from './components/mail-view/mail-view.component';
 import { MailFolderComponent } from './containers/mail-folder/mail-folder.component';
 import { MailFolderResolver } from './containers/mail-folder/mail-folder.resolve';
 import { MailService } from './mail.service';
 
 export const ROUTES: Routes = [
-  { path: 'folder/:name', component: MailFolderComponent, resolve: { messages: MailFolderResolver } }
+  {
+    path: 'folder/:name',
+    component: MailFolderComponent,
+    resolve: {
+      messages: MailFolderResolver
+    }
+  },
+  {
+    path: 'message/:id',
+    component: MailViewComponent,
+    outlet: 'pane'
+  }
 ];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(ROUTES)],
-  declarations: [MailFolderComponent, MailAppComponent, MailItemComponent],
+  declarations: [MailFolderComponent, MailAppComponent, MailItemComponent, MailViewComponent],
   providers: [MailService, MailFolderResolver],
   exports: [MailAppComponent]
 })
