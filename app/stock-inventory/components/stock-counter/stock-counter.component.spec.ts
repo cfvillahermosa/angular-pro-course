437a6ecabfa2a2a289c0a3b7f50f2ceef0e31cdb
Carlos Villa <cfvillahermosa@gmail.com>
2020-11-09 22:21:51 +0100

    Testing Component methods

---
+++ app/stock-inventory/components/stock-counter/stock-counter.component.spec.ts
@@ -0,0 +1,56 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { StockCounterComponent } from './stock-counter.component';
+
+// configure environment
+TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+
+describe('StockCounterComponent', () => {
+  // set up component
+  let component: StockCounterComponent;
+  // set up fixture wich takes our StockCounterComponent
+  let fixture: ComponentFixture<StockCounterComponent>;
+
+  beforeEach(() => {
+    // added our StockCounterComponent to our config testing module
+    TestBed.configureTestingModule({
+      declarations: [StockCounterComponent]
+    });
+
+    // overwrite variables in our before each
+    fixture = TestBed.createComponent(StockCounterComponent);
+    component = fixture.componentInstance;
+    // give an initial value (in this step we could overwrite minimum and maximum values)
+    component.value = 0;
+    component.max = 60;
+  });
+
+  it('should increment correctly', () => {
+    component.increment();
+    expect(component.value).toBe(1);
+  });
+
+  it('should decrement correctly', () => {
+    component.increment();
+    expect(component.value).toBe(1);
+    component.decrement();
+    expect(component.value).toBe(0);
+  });
+
+  it('should not decrement below the minimum value', () => {
+    component.increment();
+    expect(component.value).toBe(1);
+    component.decrement();
+    expect(component.value).toBe(0);
+
+    component.decrement();
+    expect(component.value).toBe(0);
+  });
+
+  it('should not increment below the maximum value', () => {
+    for (let i = 0; i < 200; i++) {
+      component.increment();
+    }
+    expect(component.value).toBe(60);
+  });
+});
