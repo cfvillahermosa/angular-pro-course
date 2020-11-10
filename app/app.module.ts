adeee2eb572d76a3c33f5d361a1847a8c1ecca07
Carlos Villa <cfvillahermosa@gmail.com>
2020-11-10 22:00:48 +0100

    Using InjectionToken

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { DrinkViewerComponent } from './containers/drink-viewer.component';
 import { PizzaViewerComponent } from './containers/pizza-viewer.component';
 import { SideViewerComponent } from './containers/side-viewer.component';
+import { API_TOKEN } from './token';
 
 
 
@@ -22,6 +23,8 @@ import { SideViewerComponent } from './containers/side-viewer.component';
   bootstrap: [
     AppComponent
   ],
-  providers: [{provide: 'api', useValue: '/api/pizzas'}]
+  providers: [
+    {provide: API_TOKEN, useValue: '/api/pizzas'}
+  ]
 })
 export class AppModule {}
--- app/food.service.ts
+++ app/food.service.ts
@@ -2,13 +2,13 @@ import { Inject, Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
+import { API_TOKEN } from './token';
 
 @Injectable()
 export class FoodService {
-  constructor(private http: Http, @Inject('api') private api: string) {}
+  constructor(private http: Http, @Inject(API_TOKEN) private api: string) {}
 
   getFood(): Observable<any[]> {
-    console.log('*** api', this.api);
     return this.http.get(this.api).map(response => response.json());
   }
 }
