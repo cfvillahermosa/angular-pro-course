d6834ff930e01cb70a23001ae91961ed057fdda5
Carlos Villa <cfvillahermosa@gmail.com>
2020-11-11 03:41:25 +0100

    added necessary files

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,30 +1,20 @@
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { FoodStoreModule } from './food-store/food-store.module';
 import { AppComponent } from './app.component';
-import { DrinkViewerComponent } from './containers/drink-viewer.component';
-import { PizzaViewerComponent } from './containers/pizza-viewer.component';
-import { SideViewerComponent } from './containers/side-viewer.component';
-import { API_TOKEN } from './token';
-
-
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PizzaViewerComponent,
-    DrinkViewerComponent,
-    SideViewerComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FoodStoreModule.forRoot({
+      storeId: 10292,
+      storeToken: 'eca938c99a0e9ff91029dc'
+    })
   ],
-  bootstrap: [
-    AppComponent
-  ],
-  providers: [
-    {provide: API_TOKEN, useValue: '/api/pizzas'}
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
